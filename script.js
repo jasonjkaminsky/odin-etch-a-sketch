e5e208384a37c6babf31e08f2e1cde991617087b
Jason Kaminsky <jason.j.kaminsky@gmail.com>
2022-01-04 17:15:39 -0500

    Edited bug: change Nan on line 52 to NaN.

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function resetNew() {
   ///// #5 - prompt user for how many rows in new grid /////
   let rows = prompt("Choose number of rows ( 5 - 100 ) for new box");
   console.log(rows);
-  if (isNan(rows) || rows === null) {
+  if (isNaN(rows) || rows === null) {
     return;
   }
   while (rows < 5 || rows > 100) {
