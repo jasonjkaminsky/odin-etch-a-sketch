ee08bfdf842685ba257493d70870ded0079ea235
Jason Kaminsky <jason.j.kaminsky@gmail.com>
2022-01-04 14:56:27 -0500

    A lot of JS reconfiguring

--- script.js
+++ script.js
@@ -1,28 +1,80 @@
 const container = document.querySelector(".container");
-var color = "black";
+const shakeButton = document.querySelector(".shakeButton");
 
-function createGrid(gridNumber) {
-  let gridArea = gridNumber * gridNumber;
-  for (let i = 1; i <= gridArea; i++) {
-    let gridItem = document.createElement("div");
-    container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
-    container.insertAdjacentElement("beforeend", gridItem);
+///// #2 - Sets up event listener when Shake button clicked /////
+shakeButton.addEventListener("click", resetNew);
+
+///// #1 - Makes 16 x 16 default grid /////
+function makeDefaultGrid() {
+  for (i = 0; i <= 255; i++) {
+    let defaultGrid = document.createElement("div");
+    defaultGrid.classList.add("defaultGrid");
+    defaultGrid.addEventListener("mousedown", function (event) {
+      event.target.style.backgroundColor = "black";
+    });
+    defaultGrid.addEventListener("mouseover", function (event) {
+      if (event.buttons == 1) {
+        event.target.style.backgroundColor = "black";
+      }
+    });
+    container.appendChild(defaultGrid);
   }
-  var cells = container.querySelectorAll("div");
-  cells.forEach((cell) => cell.addEventListener("mouseenter", colorGrid));
 }
 
-function colorGrid() {
-  switch (color) {
-    case "black":
-      this.style.backgroundColor = "#000000";
-      break;
+/// #4 - Removes default grid ///////
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
 }
 
-function reset(){
-  
+///// #6 - Makes a new grid after default grid is removed & user inputs rows /////
+function makeNewGrid(rows) {
+  for (i = 0; i <= rows * rows - 1; i++) {
+    let newGrid = document.createElement("div");
+    newGrid.classList.add("newGrid");
+    newGrid.addEventListener("mousedown", function (event) {
+      event.target.style.backgroundColor = "black";
+    });
+    newGrid.addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = "black";
+    });
+    container.appendChild(newGrid);
+  }
 }
 
-createGrid(30);
+///// #3 - resetNew function /////
+function resetNew() {
+  console.log("resetNew function triggered");
+  ///// #5 - prompt user for how many rows in new grid /////
+  let rows = prompt("Choose number of rows ( 5 - 100 ) for new box");
+  console.log(rows);
+  if (isNan(rows) || rows === null) {
+    return;
+  }
+  while (rows < 5 || rows > 100) {
+    alert("Rows must be between 5 and 100");
+    rows = prompt("Choose number of rows ( 5 - 100 ) for new box");
+  }
+  removeAllChildNodes(container);
+  ///// #7 - Reset the container to its original style /////
+  container.setAttribute(
+    "style",
+    `display: grid; grid-template-columns: repeat(${rows}, 1fr);
+          grid-template-rows: repeat(${rows}, 1fr); background-color: rgb(245, 245, 245); border: 4px solid rgba(105, 8, 8, 0.878);
+          border-radius: 6px;`
+  );
+
+  makeNewGrid(rows);
+}
+
+makeDefaultGrid();
+
+///// Subproblems /////
+// #1 Make 16 x 16 default grid. // ✅
+// #2 Make sure eventlistener on shake button is responding. ✅
+// #3 Make sure it runs resetNew function. ✅
+// #4 Make sure resetNew runs a function to clear default grid (removeAllChildNodes). ✅
+// #5 Prompt user for how many rows in new grid. ✅
+// #6 Execute makeNewGrid function using rows input. ✅
+// #7 Reset the container to its original style. ✅
