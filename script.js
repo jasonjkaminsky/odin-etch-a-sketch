aadf309baddc0883da5abb9f019bcbc5440161f5
Jason Kaminsky <jason.j.kaminsky@gmail.com>
2022-01-09 19:39:05 -0500

    edited rainbow and greyscale buttons

--- script.js
+++ script.js
@@ -1,79 +1,75 @@
 const container = document.querySelector(".container");
-const greyScaleButton = document.querySelector(".greyScaleButton");
-const rainbowButton = document.querySelector(".rainbowButton");
 const newGridButton = document.querySelector(".newGridButton");
 
-///// #2 - Sets up event listener when Shake button clicked /////
-greyScaleButtonadd.EventListener("click",
-rainbowButton.addEventListener("click",
+///// Sets up event listener when new grid or rainbow button clicked ////
 newGridButton.addEventListener("click", resetNew);
 
-///// #1 - Makes 16 x 16 default grid /////
+///// Makes 16 x 16 default grid /////
 function makeDefaultGrid() {
-  for (i = 0; i <= 399; i++) {
-    let defaultGrid = document.createElement("div");
-    defaultGrid.classList.add("defaultGrid");
-    defaultGrid.addEventListener("mousedown", function (event) {
-      event.target.style.backgroundColor = "black";
-    });
-    defaultGrid.addEventListener("mouseover", function (event) {
-      if (event.buttons == 1) {
-        event.target.style.backgroundColor = "black";
-      }
-    });
-    container.appendChild(defaultGrid);
-  }
+    for (i = 0; i <= 899; i++) {
+        let defaultGrid = document.createElement("div");
+        defaultGrid.classList.add("defaultGrid");
+        defaultGrid.addEventListener("mousedown", function (event) {
+            event.target.style.backgroundColor = "black";
+        });
+        defaultGrid.addEventListener("mouseover", function (event) {
+            if (event.buttons == 1) {
+                event.target.style.backgroundColor = "black";
+            }
+        });
+        container.appendChild(defaultGrid);
+    }
 }
 
-/// #4 - Removes default grid ///////
+///// Removes default grid /////
 function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
-///// #6 - Makes a new grid after default grid is removed & user inputs rows /////
+///// Makes a new grid after default grid is removed & user inputs rows /////
 function makeNewGrid(rows) {
-  for (i = 0; i <= rows * rows - 1; i++) {
-    let newGrid = document.createElement("div");
-    newGrid.classList.add("newGrid");
-    newGrid.addEventListener("mousedown", function (event) {
-      event.target.style.backgroundColor = "black";
-    });
-    newGrid.addEventListener("mouseover", function (event) {
-      if (event.buttons == 1) {
-        event.target.style.backgroundColor = "black";
-      }
-    });
-    container.appendChild(newGrid);
-  }
+    for (i = 0; i <= rows * rows - 1; i++) {
+        let newGrid = document.createElement("div");
+        newGrid.classList.add("newGrid");
+        newGrid.addEventListener("mousedown", function (event) {
+            event.target.style.backgroundColor = "black";
+        });
+        newGrid.addEventListener("mouseover", function (event) {
+            if (event.buttons == 1) {
+                event.target.style.backgroundColor = "black";
+            }
+        });
+        container.appendChild(newGrid);
+    }
 }
 
-///// #3 - resetNew function /////
+///// resetNew function /////
 function resetNew() {
-  console.log("resetNew function triggered");
-  ///// #5 - prompt user for how many rows in new grid /////
-  let rows = prompt("Choose number of rows ( 5 - 100 ) for new box");
-  console.log(rows);
+    console.log("resetNew function triggered");
+    ///// prompt user for how many rows in new grid /////
+    let rows = prompt("Choose number of rows ( 5 - 100 ) for new box");
+    console.log(rows);
 
-  while (rows < 5 || rows > 100) {
-    alert("Rows must be between 5 and 100");
-    rows = prompt("Choose number of rows ( 5 - 100 ) for new box");
-  }
-  if (isNaN(rows) || rows === null) {
-    return;
-  }
+    while (rows < 5 || rows > 100) {
+        alert("Rows must be between 5 and 100");
+        rows = prompt("Choose number of rows ( 5 - 100 ) for new box");
+    }
+    if (isNaN(rows) || rows === null) {
+        return;
+    }
 
-  removeAllChildNodes(container);
-  ///// #7 - Reset the container to its original style /////
-  container.setAttribute(
-    "style",
-    `display: grid; grid-template-columns: repeat(${rows}, 1fr);
+    removeAllChildNodes(container);
+    /////  Reset the container to its original style /////
+    container.setAttribute(
+        "style",
+        `display: grid; grid-template-columns: repeat(${rows}, 1fr);
           grid-template-rows: repeat(${rows}, 1fr); background-color: rgb(245, 245, 245); border: 4px solid rgba(105, 8, 8, 0.878);
           border-radius: 6px;`
-  );
+    );
 
-  makeNewGrid(rows);
+    makeNewGrid(rows);
 }
 
 makeDefaultGrid();
